84e5afe3072bffecdecfd8668ae329b78b749198
CharaD7 <jijakahn6@gmail.com>
2023-01-28 15:01:57 +0000

    Customized nav

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <div>Hello</div>
+    <Nav />
+    <div class="text-center">
+      <h1 class="mb-4 mt-5r">Welcome to Restaurantly</h1>
+      <a href="/restaurants">Go to restaurants</a>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.mt-5r {
+  margin-top: 5rem;
+}
+</style>
