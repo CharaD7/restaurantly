ef88b96363d496d95c1f0efc33defb6c8b35a0d6
CharaD7 <jijakahn6@gmail.com>
2023-01-31 21:59:44 +0000

    Added restaurantly page

---
+++ pages/restaurants.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Nav />
+    <div class="text-center">
+      <h1 class="mb-4 mt-5r">This is Restaurantly</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.mt-5r {
+  margin-top: 5rem;
+}
+</style>
